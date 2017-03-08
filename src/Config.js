8b6b489fa09f02d517db676f53a11b555bfc586d
xuweichen <xuweichen@meitu.io>
2017-03-08 15:14:23 +0800

    merge conflict

--- src/Config.js
+++ src/Config.js
@@ -1,6 +1,6 @@
 /**
  * export default require('${RUNtIME_ENV}');
  */
-import Config from './config/dev';
-
+//import Config from './config/dev';
+import Config from './config/prod';
 export default Config;
